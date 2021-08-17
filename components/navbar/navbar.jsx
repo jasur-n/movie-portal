fc5ba442b681e0ad13194a96e839d179920723f4
normuradoff <normuradoff1@gmail.com>
2021-08-17 13:23:25 +0500

    add a navbar component

---
+++ components/navbar/navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar() {
+  return (
+    <nav>
+      <img
+        className="block h-8 w-auto"
+        src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+        alt="Workflow"
+      />
+    </nav>
+  );
+}
