c8b03703a67297f1b033dc44738c0ba01cb7ddab
normuradoff <normuradoff1@gmail.com>
2021-08-17 18:35:41 +0500

    feat(componenst): connect pagination component

--- components/pagination/pagination.jsx
+++ components/pagination/pagination.jsx
@@ -1,79 +1,170 @@
-/* This example requires Tailwind CSS v2.0+ */
-import {
-  ArrowNarrowLeftIcon,
-  ArrowNarrowRightIcon
-} from '@heroicons/react/solid';
+import React from 'react';
+import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/solid';
+import classnames from 'classnames';
+import PropTypes from 'prop-types';
 
-export default function Example() {
+const Pagination = ({
+  currentPage,
+  setCurrentPage,
+  itemsNumber,
+  itemsPerPage
+}) => {
+  const maxEl =
+    itemsPerPage * currentPage < itemsNumber
+      ? itemsPerPage * currentPage
+      : itemsNumber;
+  const minEl = itemsPerPage * (currentPage - 1) + 1;
+  const pagesNumber =
+    itemsNumber === itemsPerPage
+      ? 1
+      : Math.floor(itemsNumber / itemsPerPage) + 1;
   return (
-    <nav className="border-t border-gray-200 px-4 flex items-center justify-between sm:px-0">
-      <div className="-mt-px w-0 flex-1 flex">
-        <a
-          href="#"
-          className="border-t-2 border-transparent pt-4 pr-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
+    <div className="bg-white px-4 py-3 flex items-center justify-between border-t border-gray-favorite sm:px-6">
+      <div className="flex-1 flex justify-between sm:hidden">
+        <button
+          type="button"
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+          className="disabled:opacity-50 relative inline-flex items-center px-4 py-2 border border-gray-favorite text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
         >
-          <ArrowNarrowLeftIcon
-            className="mr-3 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
           Previous
-        </a>
-      </div>
-      <div className="hidden md:-mt-px md:flex">
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-        >
-          1
-        </a>
-        {/* Current: "border-indigo-500 text-indigo-600", Default: "border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300" */}
-        <a
-          href="#"
-          className="border-indigo-500 text-indigo-600 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-          aria-current="page"
-        >
-          2
-        </a>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-        >
-          3
-        </a>
-        <span className="border-transparent text-gray-500 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium">
-          ...
-        </span>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-        >
-          8
-        </a>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-        >
-          9
-        </a>
-        <a
-          href="#"
-          className="border-transparent text-gray-500 hover:text-gray-700 hover:border-gray-300 border-t-2 pt-4 px-4 inline-flex items-center text-sm font-medium"
-        >
-          10
-        </a>
-      </div>
-      <div className="-mt-px w-0 flex-1 flex justify-end">
-        <a
-          href="#"
-          className="border-t-2 border-transparent pt-4 pl-1 inline-flex items-center text-sm font-medium text-gray-500 hover:text-gray-700 hover:border-gray-300"
+        </button>
+        <button
+          type="button"
+          onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+          disabled={currentPage === pagesNumber}
+          className="disabled:opacity-50 ml-3 relative inline-flex items-center px-4 py-2 border border-gray-favorite text-sm font-medium rounded-md text-gray-700 bg-white hover:bg-gray-50"
         >
           Next
-          <ArrowNarrowRightIcon
-            className="ml-3 h-5 w-5 text-gray-400"
-            aria-hidden="true"
-          />
-        </a>
+        </button>
       </div>
-    </nav>
+      <div className="hidden sm:flex-1 sm:flex flex-wrap-reverse sm:items-center sm:justify-between">
+        <div className="whitespace-nowrap mt-2">
+          <p className="text-sm text-gray-700">
+            Showing <span className="font-medium">{minEl}</span> to{' '}
+            <span className="font-medium">{maxEl}</span> of{' '}
+            <span className="font-medium">{itemsNumber}</span> results
+          </p>
+        </div>
+        <div>
+          <nav
+            className="relative z-0 inline-flex rounded-md shadow-sm -space-x-px"
+            aria-label="Pagination"
+          >
+            <button
+              type="button"
+              disabled={currentPage === 1}
+              onClick={() => setCurrentPage((prevPage) => prevPage - 1)}
+              className="disabled:opacity-50 relative inline-flex items-center px-2 py-2 rounded-l-md border border-gray-favorite bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            >
+              <span className="sr-only">Previous</span>
+              <ChevronLeftIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+            {/* Current: "z-10 bg-indigo-50 border-indigo-500 text-indigo-600", Default: "bg-white border-gray-favorite text-gray-500 hover:bg-gray-50" */}
+            <button
+              type="button"
+              aria-current="page"
+              onClick={() => setCurrentPage(1)}
+              className={classnames(
+                {
+                  'z-10 bg-indigo-50 border-indigo-500 text-indigo-500 ':
+                    currentPage === 1
+                },
+                {
+                  'bg-white border-gray-favorite text-gray-500 hover:bg-gray-50':
+                    currentPage !== 1
+                },
+                'relative inline-flex items-center px-4 py-2 border text-sm font-medium'
+              )}
+            >
+              1
+            </button>
+            {currentPage > 3 && (
+              <span className="relative inline-flex items-center px-4 py-2 border border-gray-favorite bg-white text-sm font-medium text-gray-700">
+                ...
+              </span>
+            )}
+            {Array(pagesNumber)
+              .fill(1)
+              .map((el, index) => {
+                const pageNum = index + 1;
+                const show =
+                  pageNum !== 1 &&
+                  pageNum !== pagesNumber &&
+                  (pageNum === currentPage - 1 ||
+                    pageNum === currentPage + 1 ||
+                    pageNum === currentPage);
+
+                return (
+                  show && (
+                    <button
+                      key={pageNum}
+                      type="button"
+                      aria-current="page"
+                      onClick={() => setCurrentPage(pageNum)}
+                      className={classnames(
+                        {
+                          'z-10 bg-indigo-50 border-indigo-500 text-indigo-500 ':
+                            pageNum === currentPage
+                        },
+                        {
+                          'bg-white border-gray-favorite text-gray-500 hover:bg-gray-50':
+                            currentPage !== pageNum
+                        },
+                        'relative inline-flex items-center px-4 py-2 border text-sm font-medium'
+                      )}
+                    >
+                      {pageNum}
+                    </button>
+                  )
+                );
+              })}
+            {currentPage < pagesNumber - 2 && (
+              <span className="relative inline-flex items-center px-4 py-2 border border-gray-favorite bg-white text-sm font-medium text-gray-700">
+                ...
+              </span>
+            )}
+            <button
+              type="button"
+              aria-current="page"
+              onClick={() => setCurrentPage(pagesNumber)}
+              className={classnames(
+                {
+                  'z-10 bg-indigo-50 border-indigo-500 text-indigo-500':
+                    currentPage === pagesNumber
+                },
+                {
+                  'bg-white border-gray-favorite text-gray-500 hover:bg-gray-50':
+                    currentPage !== pagesNumber
+                },
+                'relative inline-flex items-center px-4 py-2 border text-sm font-medium'
+              )}
+            >
+              {pagesNumber}
+            </button>
+
+            <button
+              type="button"
+              onClick={() => setCurrentPage((prevPage) => prevPage + 1)}
+              disabled={currentPage === pagesNumber}
+              className="disabled:opacity-50 relative inline-flex items-center px-2 py-2 rounded-r-md border border-gray-favorite bg-white text-sm font-medium text-gray-500 hover:bg-gray-50"
+            >
+              <span className="sr-only">Next</span>
+              <ChevronRightIcon className="h-5 w-5" aria-hidden="true" />
+            </button>
+          </nav>
+        </div>
+      </div>
+    </div>
   );
-}
+};
+Pagination.propTypes = {
+  currentPage: PropTypes.number.isRequiindigo,
+  setCurrentPage: PropTypes.func.isRequiindigo,
+  itemsNumber: PropTypes.number.isRequiindigo,
+  itemsPerPage: PropTypes.number.isRequiindigo
+};
+
+Pagination.defaultProps = {};
+
+export default Pagination;
--- pages/index.js
+++ pages/index.js
@@ -3,27 +3,44 @@ import { useEffect, useState } from 'react';
 import axios from 'axios';
 import Layout from '../components/layout';
 import MovieList from '../components/movie-list';
+import Pagination from '../components/pagination';
 
 export default function Home() {
   const [movies, setMovies] = useState([]);
+  const [moviesTotalNumber, setMoviesTotalNumber] = useState();
+  const [itemsPerPage, setItemsPerPage] = useState(10);
+  const [currentPage, setCurrentPage] = useState(1);
   useEffect(() => {
     axios
       .get(
-        'https://api.itv.uz/api/content/main/2/list?user=4bb5a3841629114633e611b7590584ec044'
+        `https://api.itv.uz/api/content/main/2/list?page=${currentPage}&user=4bb5a3841629114633e611b7590584ec044`
       )
-      .then((response) => setMovies(response.data.data.movies))
+      .then((response) => {
+        const { data } = response.data;
+        setMovies(data.movies);
+        setMoviesTotalNumber(data.total_items);
+        setItemsPerPage(data.items_per_page);
+      })
       .catch(() => {
         throw new Error('Something went wrong while fetching movies...');
       });
-  }, []);
+  }, [currentPage]);
 
   return (
     <Layout>
       <Head>
-        <title>Movie Portal</title>
+        <title>Movies</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <MovieList title="Movies" items={movies} />
+      {moviesTotalNumber && (
+        <Pagination
+          currentPage={currentPage}
+          setCurrentPage={setCurrentPage}
+          itemsNumber={moviesTotalNumber}
+          itemsPerPage={itemsPerPage}
+        />
+      )}
     </Layout>
   );
 }
