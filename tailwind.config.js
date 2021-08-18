ff60d494b5bca6585392107ce84b9501c869f574
normuradoff <normuradoff1@gmail.com>
2021-08-18 09:45:05 +0500

    refactor: disable JIT mode of tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,5 @@
 /* eslint-disable global-require */
 module.exports = {
-  mode: 'jit',
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
